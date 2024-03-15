eca1e2ae6a21ea10fae4f511cdff1864aa3ce2f9
Curtis Kretzer <curtiskretzer@gmail.com>
2024-03-14 19:58:18 -0500

    Work damn it

--- index.js
+++ index.js
@@ -1,50 +1,29 @@
 addEventListener("load", async e => {
-    function gallery_refresh() {
-        let galleryContent = document.querySelector(".gallery-content");
-        galleryContent.innerHTML = "";
+    let gallery;
 
-        load_images("./gallery/Important"+Math.floor(Math.random() * 17 + 1),
-            images => {
-                let i = images.length;
-                while(i--) {
-                    images[i].className = "gallery-img";
-                    galleryContent.appendChild(images[i]);
-                }
-            });
-    };
-    
-    document.querySelector(".gallery-title").addEventListener("click", gallery_refresh, false);
-    gallery_refresh();
-}, false);
+    await fetch("gallery/manifest.json")
+        .then(response => response.json())
+        .then(data => gallery = data);
 
-/**
- * Loads all images from a folder and returns them
- * @param {string} folder_url the folder to load images from
- * @returns {Array<HTMLImageElement>}
- */
-async function load_images(folder_url, after_load_callback) {
-    let request = new XMLHttpRequest();
-    request.open("GET", folder_url, true);
-    request.responseType = 'document';
+    document.querySelector(".gallery-title").addEventListener("click", loadImages, false);
+    document.querySelector(".title").addEventListener("click", onClickTitle, false);
 
-    let images = [];
+    function onClickTitle(e) {
+        let targetElement = document.querySelector(".mb-center");
+        targetElement.innerHTML += moby[targetElement.innerHTML.length] || "";
+    }
 
-    request.onload = _ => {
-        if(request.status === 200) { // HTML status 200 OK
-            let elements = request.response.getElementsByTagName("a");
-            for(let a of elements) {
-                if(a.href.match(/\.(jpe?g|png|gif|jfif)$/)) { 
-                    let img = new Image(); // create a new image
-                    img.src = a.href; // the url of the file
-                    images.push(img); // put the image into the array
-                } 
-            }
-        } else {
-            throw('Request failed. Returned status of ' + request.status);
+    function loadImages(e) {
+        let targetElement = document.querySelector(".gallery-content");
+        let Path = "Important" + Math.floor(Math.random() * 17 + 1);
+
+        targetElement.innerHTML = "";
+        for(let x of gallery[Path]) {
+            let img = document.createElement("img");
+            img.className = "gallery-img";
+            img.src = "./gallery/"+Path+"/"+x;
+            targetElement.appendChild(img);
         }
-        after_load_callback(images);
-    };
-
-    request.send();
-    return images;
-}
+    }
+    loadImages();
+}, false);
